62e52ba2ef757eea422cdf68f73d33ca2cf252b9
isaac <tiezic@gmail.com>
2026-02-22 05:07:02 +0000

    added main method and node methods with no implementation yet

---
+++ NodeTestMain.java
@@ -0,0 +1,22 @@
+
+/**
+ * In main, we have to:
+ * 1. create an empty list
+ * 2. insert three nodes
+ * 3. print the list
+ * 4. search for a node and print 
+ *    if it was found or not
+ * 5. delete one node
+ * 6. print list to show deletion
+ */
+public class NodeTestMain {
+    public static void main(String[] args) {
+        //1. node without value or pointer; empty list so far
+        Node n1 = new Node();
+        n1.key = (Integer) null;
+        n1.next = null;
+
+        //2. insert three nodes into list
+        //n1.listInsert(1, n1)
+    }
+}
